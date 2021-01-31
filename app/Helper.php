b989319495a443e8a76f1ee71097c748cbba1b35
Kenneth Laqui <kennethlaqui@yahoo.com>
2021-01-31 15:21:12 +0800

    Add geolocation

---
+++ app/Helper.php
@@ -0,0 +1,35 @@
+<?php 
+
+// Get Geolocation
+
+function get_local_time(){
+
+    $ip = @file_get_contents("http://ipecho.net/plain");
+
+    if ($ip === false) { return $timezone = "GMT+8"; }
+
+    $url = 'http://ip-api.com/json/'.$ip;
+    
+    $timezone = file_get_contents($url);
+
+    $timezone = json_decode($timezone,true)['timezone'];
+
+    return $timezone;
+
+}
+
+function get_ip_location() {
+
+    $ip = @file_get_contents("http://ipecho.net/plain");
+
+    if ($ip === false) { return $country = "N/A"; }
+
+    $url = 'http://ip-api.com/json/'.$ip;
+    
+    $country = file_get_contents($url);
+
+    $country = json_decode($country,true)['country'];
+
+    return $country;
+
+}
