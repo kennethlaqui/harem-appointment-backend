c18670e00208e5c46477d78168a3005af59bff61
Kenneth Laqui <kennethlaqui@yahoo.com>
2021-01-31 15:20:49 +0800

    Refactor Appointment.php

--- app/Http/Controllers/Appointment.php
+++ app/Http/Controllers/Appointment.php
@@ -12,47 +12,63 @@ class Appointment extends Controller
     public function locations ()
     {
 
-        $locations = DB::table('c_locn_cde')
+        return DB::table('c_locn_cde')
             ->select('locn_cde', 'locn_nme')
+            ->where('stor_nme', env('STORE_NAME'))
+            ->distinct()
             ->get();
-
-        return response()->json($locations, 200);
-
-        // return response()->json($locations, 200);
-        
-        // return View::make('example', ['locations' => $locations]);
-
-    }
-
-    public function findTheLocation (Request $request)
-    {
-
-        $locations = DB::table('c_locn_cde')
-            ->where('locn_cde', $request->locn_cde)
-            ->value('locn_cde')
-            ->get();
-        
-        return View::make('example', ['locations' => $locations]);
 
     }
 
     public function time (Request $request)
     {
 
-        $time = DB::table('c_validtme')
+        return DB::table('c_validtme')
             ->where('locn_cde', $request->locn_cde)
             ->where('day_numb', $request->day_numb)
             ->select('mil_time', 'std_time')
             ->get();
-        
-        return response()->json($time, 200);
 
     }
 
     public function save(Request $request)
     {
 
-        $mil_time = '23:59';
+        $cntrl_no = $this->maximum_control_number();
+
+        DB::table('c_appointm')
+            ->insert([
+                'cntrl_no' => $cntrl_no,
+                'log_date' => Carbon::now(get_local_time()),
+                'log_time' => Carbon::now(get_local_time())->isoFormat('HH:mm'),
+                'clnt_nme' => $request->clnt_nme,
+                'apnt_dte' => $request->apnt_dte,
+                'apnt_tme' => '09:00 AM',
+                'mil_time' => $request->apnt_tme,
+                'locn_cde' => $request->locn_cde,
+                'therapst' => $request->therapst,
+                'treatmnt' => $request->treatmnt,
+                'emailadd' => $request->emailadd,
+                'cel_numb' => $request->cel_numb,
+                'location' => get_ip_location()
+        ]);
+
+    }
+
+    public function appointments (Request $request)
+    {
+
+        return DB::table('c_appointm')
+            ->join('c_locn_cde', 'c_locn_cde.locn_cde', '=', 'c_appointm.locn_cde')
+            ->select('c_appointm.*', 'c_locn_cde.locn_nme')
+            ->orderBy('cntrl_no', 'DESC')
+            ->get();
+
+    }
+
+    public function maximum_control_number ()
+    {
+
         $cntrl_no = DB::table('c_appointm')
             ->max('cntrl_no');
 
@@ -60,33 +76,7 @@ class Appointment extends Controller
             $cntrl_no=0;
         }
 
-        $cntrl_no++;
-        $log_time ='11:23:58';
-
-        DB::table('c_appointm')
-            ->insert([
-                'cntrl_no' => $cntrl_no,
-                'log_date' => Carbon::now(),
-                'log_time' => $log_time,
-                'clnt_nme' => $request->clnt_nme,
-                'apnt_dte' => $request->apnt_dte,
-                'mil_time' => $request->apnt_tme,
-                'locn_cde' => $request->locn_cde,
-                'therapst' => $request->therapst,
-                'treatmnt' => $request->treatmnt,
-                'emailadd' => $request->emailadd,
-                'cel_numb' => $request->cel_numb
-        ]);
-
-    }
-
-    public function appointments ()
-    {
-
-        return DB::table('c_appointm')
-            ->join('c_locn_cde', 'c_locn_cde.locn_cde', '=', 'c_appointm.locn_cde')
-            ->select('c_appointm.*', 'c_locn_cde.locn_nme')
-            ->get();
+        return $cntrl_no += 1;
 
     }
 }
